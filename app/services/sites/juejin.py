1c696e44b967324dfd423c03bdbfbdb07c069e70
Gang <deepincode@qq.com>
2025-09-28 00:41:42 +0800

    fix: 修复微博

--- app/services/sites/juejin.py
+++ app/services/sites/juejin.py
@@ -20,17 +20,9 @@ class JueJinCrawler(Crawler):
         # 获取当前时间
         current_time = datetime.datetime.now()
         
-        url = "https://api.juejin.cn/recommend_api/v1/article/recommend_all_feed"
+        url = "https://api.juejin.cn/content_api/v1/content/article_rank?category_id=1&type=hot"
         
-        payload = {
-            "id_type": 2,
-            "client_type": 2608,
-            "sort_type": 3,
-            "cursor": "0",
-            "limit": 20
-        }
-        
-        resp = requests.post(url=url, json=payload, headers=self.header, verify=False, timeout=self.timeout)
+        resp = requests.get(url=url,  headers=self.header, verify=False, timeout=self.timeout)
         if resp.status_code != 200:
             print(f"request failed, status: {resp.status_code}")
             return []
@@ -43,16 +35,15 @@ class JueJinCrawler(Crawler):
             cache_list = []
             
             for item in data:
-                article_info = item.get('article_info', {})
+                article_info = item.get('content', {})
                 title = article_info.get('title', '')
-                article_id = article_info.get('article_id', '')
+                article_id = article_info.get('content_id', '')
                 url = f"https://juejin.cn/post/{article_id}"
-                brief = article_info.get('brief_content', '')
-                
+
                 news = {
                     'title': title,
                     'url': url,
-                    'content': brief,
+                    'content': title,
                     'source': 'juejin',
                     'publish_time': current_time.strftime('%Y-%m-%d %H:%M:%S')
                 }
